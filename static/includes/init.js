afa8bc5729f77d5dbd835efd5655d4e1c4cec40a
tayabsoomro <isoomro.t@gmail.com>
2017-11-30 12:57:34 -0600

    static

--- static/includes/init.js
+++ static/includes/init.js
@@ -1,5 +1,5 @@
 
 // load common code that includes config, then load the app logic for this page.
 requirejs(['./common'], function(common){
-    requirejs(['/static/includes/app/main']);
+    requirejs(['{{ url_of("static",filename="./includes/app/main") }}']);
 });
