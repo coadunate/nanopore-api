a4849102bda7c124ba35af9dacd291f971824fec
= <=>
2017-11-30 17:57:44 +0000

    fix server issue

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from resources import ReadTableResource
 from resources import ReadResource
 from resources import EventResource
+#from resources import FrontEndResource
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,5 +13,10 @@ api.add_resource(ReadTableResource, '/tabledata', endpoint='tabledata')
 api.add_resource(ReadResource, '/readdata/<string:qname>', endpoint='readdata')
 api.add_resource(EventResource, '/eventdata/<string:qname>', endpoint='eventdata')
 
+
+@app.route('/')
+def index():
+	return "Hello World!"
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='192.168.64.11',port=80,debug=True)
