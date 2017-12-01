96b38e1272857166f153657588bc902aa9b53855
tayabsoomro <isoomro.t@gmail.com>
2017-11-30 23:06:29 -0600

    fixing scales

--- static/includes/lib/modules/scales.js
+++ static/includes/lib/modules/scales.js
@@ -11,6 +11,6 @@ define(function(require){
         ySG: d3.scaleLinear().range([utils.height, 0]), // the y-scale for signal graph.
         ymSG: d3.scaleLinear().range([utils.heightminiSignalGraph, 0]), // the y-scale for mini signal graph.
         xR: d3.scaleLinear().range([0, utils.width]), // the x-scale for read alignment viewer.
-        yR: d3.scaleLinear().range([utils.height, 0]) // the y-scale for the read alignment viewer.
+        yR: d3.scaleLinear().range([100, 0]) // the y-scale for the read alignment viewer.
     };
 });
